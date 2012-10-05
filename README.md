abf1a23d5086d5bb7282f972a9ebbe0ae7df76a7
Eric Mann <eric@eamann.com>
2012-10-05 15:15:31 -0700

    Initial Release
    Might still be buggy, but from what I can tell it accurately detects WordPress and runs PHP commands as expected.  The `man` command does not work on Windows, so some help functionality is still broken.

--- README.md
+++ README.md
@@ -3,6 +3,19 @@ WP-PowerShell
 
 Windows PowerShell wrapper for WordPress CLI tools.
 
+Requirements
+------------
+
+* PHP >= 5.3
+* WP >= 3.3
+
+Installing
+----------
+
+Clone the repository into [User]/Documents/WindowsPowerShell/Modules.
+
+Run `Import-Module WP-PowerShell` from the PowerShell prompt.
+
 Using
 -----
 
@@ -11,3 +24,33 @@ Navigate to a WordPress root folder within Windows:
 ```
 cd C:\xampp\htdocs\wp
 ```
+
+Enclose multiple CLI commands in quotes.  For example:
+
+```
+wp "home help"
+```
+
+Typing `wp help` should show you output similar to this:
+
+```
+Example usage:
+	wp google-sitemap [build|help] ...
+	wp core [update|help] ...
+	wp home [help] ...
+	wp option [add|update|delete|get|help] ...
+	wp plugin [status|activate|deactivate|install|delete|update|help] ...
+	wp theme [status|details|activate|help] ...
+```
+
+Changelog
+---------------
+
+**1.0**
+
+- Initial release
+
+Contributors
+------------
+
+- [Eric Mann](http://eamann.com)
--- WP-PowerShell.psm1
+++ WP-PowerShell.psm1
@@ -1,5 +1,18 @@
 # Copyright 2012 Eric Mann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 # 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 Function WP 
 {
@@ -11,22 +24,23 @@ Function WP
     .EXAMPLE
         WP "home help"
         
-        WP "home help" "c:\xampp\php"
-        
-        WP "home help" -cliPath "C:\xampp\php"
-        
-        WP -commands "home help" -cliPath "C:\xampp\php"
+        WP "home help" "c:\tools\wp-cli" "c:\xampp\htdocs\wordpress"
+              
+        WP -commands "home help" -cliPath "C:\tools\wp-cli" -wpPath "C:\xampp\htdocs\wordpress"
     #>
 	param( 
-        [string] $commands,
-        [string] $cliPath
+        [string[]] $commands,
+        [string] $cliPath,
+        [string] $wpPath = "C:\xampp\htdocs\wp-test"
     )
 	
 	$scriptPath = Get-PathToCli $cliPath
     
     $cli = Join-Path $scriptPath \wp-cli\wp-cli-boot.php
     
-    php $cli $commands
+    $commandArray = $commands -split " "
+       
+    php $cli $commandArray --path=`"$wpPath`"
 }
 
 Function Get-PathToCli
